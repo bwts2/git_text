bf34e4c4f024375c99cb1b4bfc0ad3ef1cc69d7b
sq <2100628828@qq.com>
2022-10-22 19:24:43 +0800

    主分支 第二次提交

---
+++ src/main/java/com/zl/git/GitText.java
@@ -0,0 +1,15 @@
+package com.zl.git;
+
+/**
+ * @类名: GitText
+ * @作者: SQ
+ * @系统: SQS
+ * @时间: 2022/10/22 19:22
+ * @return:
+ * @作用：测试git
+ **/
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
