5f5636dcef2954772b81d1dd42c58be1eb7d6351
sq <2100628828@qq.com>
2022-10-22 19:30:45 +0800

    dev分支 第一次提交

--- src/main/java/com/zl/git/GitText.java
+++ src/main/java/com/zl/git/GitText.java
@@ -13,5 +13,7 @@ public class GitText {
         System.out.println("hello git");
         System.out.println("=========");
         System.out.println("新加修改");
+        System.out.println("=========");
+        System.out.println("dev 分支第一次修改");
     }
 }
