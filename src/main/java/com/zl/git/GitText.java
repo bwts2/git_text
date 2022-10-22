70714d555ddcb1210a7187c968c7e67a5d6ca902
sq <2100628828@qq.com>
2022-10-22 19:32:13 +0800

    主分支 第四次提交 异常提交

--- src/main/java/com/zl/git/GitText.java
+++ src/main/java/com/zl/git/GitText.java
@@ -15,5 +15,7 @@ public class GitText {
         System.out.println("新加修改");
         System.out.println("=========");
         System.out.println("dev 分支第一次修改");
+        System.out.println("=========");
+        System.out.println("修改异常");
     }
 }
